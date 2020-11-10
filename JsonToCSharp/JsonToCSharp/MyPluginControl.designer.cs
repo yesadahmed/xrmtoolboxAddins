ea469fd355acd5252b04f5b8a088945bfb2dbbfd
Adnan Samuel <yesadahmed@gmail.com>
2020-11-10 14:41:41 +0100

    Added json to CSharp addin for xrmtoolbox

---
+++ JsonToCSharp/JsonToCSharp/MyPluginControl.designer.cs
@@ -0,0 +1,160 @@
+﻿namespace JsonToCSharp
+{
+    partial class MyPluginControl
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblClose = new System.Windows.Forms.Label();
+            this.cmbEntities = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBoxSource = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBoxDest = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblClose
+            // 
+            this.lblClose.AutoSize = true;
+            this.lblClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClose.Location = new System.Drawing.Point(1011, 5);
+            this.lblClose.Name = "lblClose";
+            this.lblClose.Size = new System.Drawing.Size(99, 13);
+            this.lblClose.TabIndex = 0;
+            this.lblClose.Text = "Close the Plugin";
+            this.lblClose.Click += new System.EventHandler(this.lblClose_Click);
+            // 
+            // cmbEntities
+            // 
+            this.cmbEntities.FormattingEnabled = true;
+            this.cmbEntities.Location = new System.Drawing.Point(67, 14);
+            this.cmbEntities.Name = "cmbEntities";
+            this.cmbEntities.Size = new System.Drawing.Size(271, 21);
+            this.cmbEntities.TabIndex = 1;
+            this.cmbEntities.SelectedIndexChanged += new System.EventHandler(this.cmbEntities_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Entities:";
+            // 
+            // richTextBoxSource
+            // 
+            this.richTextBoxSource.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.richTextBoxSource.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBoxSource.Location = new System.Drawing.Point(15, 87);
+            this.richTextBoxSource.Name = "richTextBoxSource";
+            this.richTextBoxSource.Size = new System.Drawing.Size(594, 520);
+            this.richTextBoxSource.TabIndex = 4;
+            this.richTextBoxSource.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Source Json";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // richTextBoxDest
+            // 
+            this.richTextBoxDest.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBoxDest.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBoxDest.Location = new System.Drawing.Point(642, 87);
+            this.richTextBoxDest.Name = "richTextBoxDest";
+            this.richTextBoxDest.Size = new System.Drawing.Size(516, 520);
+            this.richTextBoxDest.TabIndex = 6;
+            this.richTextBoxDest.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(868, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "CSharp Class";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.Location = new System.Drawing.Point(356, 10);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(94, 27);
+            this.btnConvert.TabIndex = 9;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // MyPluginControl
+            // 
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBoxDest);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBoxSource);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbEntities);
+            this.Controls.Add(this.lblClose);
+            this.Name = "MyPluginControl";
+            this.Size = new System.Drawing.Size(1172, 629);
+            this.OnCloseTool += new System.EventHandler(this.MyPluginControl_OnCloseTool_1);
+            this.Load += new System.EventHandler(this.MyPluginControl_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStrip toolStripMenu;
+        private System.Windows.Forms.ToolStripButton tsbClose;
+        private System.Windows.Forms.ToolStripButton tsbSample;
+        private System.Windows.Forms.ToolStripSeparator tssSeparator1;
+        private System.Windows.Forms.Label lblClose;
+        private System.Windows.Forms.ComboBox cmbEntities;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBoxSource;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBoxDest;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
